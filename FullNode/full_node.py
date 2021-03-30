feba329108c5c33f86b6b944d73dcf809e0dcadd
OrenSitton <64351837+OrenSitton@users.noreply.github.com>
2021-03-31 01:53:43 +0300

    added multi-threaded client connections

--- FullNode/full_node.py
+++ FullNode/full_node.py
@@ -7,13 +7,13 @@ Description:
 import socket
 import select
 import logging
-from time import sleep
 import threading
-from synced_array import SyncedArray
+from time import sleep
 
+from Classes.SyncedArray import SyncedArray
+from Classes.Blockchain import Blockchain
 
-def open_configuration():
-    pass
+client_sockets = SyncedArray(name="client list")
 
 
 def initiate_server(ip, port):
@@ -32,6 +32,37 @@ def initiate_server(ip, port):
     server_socket.bind((ip, port))
     server_socket.listen(5)
     return server_socket
+
+
+def initiate_client(ip, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client_socket.connect((ip, port))
+        logging.info("Connected to node [{}, {}]"
+                     .format(client_socket.getpeername()[0], client_socket.getpeername()[1]))
+
+    except ConnectionRefusedError:
+        logging.info("Connection attempt refused by node [{}, {}]"
+                     .format(ip, port))
+
+    else:
+        client_sockets.append(client_socket)
+
+
+def initiate_clients(addresses, port):
+    threads = []
+    for i, address in enumerate(addresses):
+        thread = threading.Thread(name="client connection attempt {}".format(i+1), target=initiate_client, args=(address, port, ))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    logging.info("{} nodes accepted connection"
+                 .format(len(client_sockets)))
+
 
 
 def peer_seed(ip, port, **kwargs):
@@ -71,47 +102,40 @@ def main():
     port = 8334
     seed_ip = "localhost"
     seed_port = 8333
-    client_addresses = []
 
     # initialize blockchain & transactions list
 
     # initiate server socket
-    server = initiate_server(ip, port)
+    server_socket = initiate_server(ip, port)
     logging.info("Server initiated [{}, {}]"
-                 .format(server.getsockname()[0], server.getsockname()[1]))
+                 .format(server_socket.getsockname()[0], server_socket.getsockname()[1]))
 
     # initiate inputs list (from config file or from seed server)
     peer_addresses = peer_seed(seed_ip, seed_port, attempts=1)
     logging.info("Seeding yielded {} addresses".format(len(peer_addresses)))
 
-    client_addresses += peer_addresses
-
     # initiate clients
-    clients = []
-
-    for address in client_addresses:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clients.append(client)
-        try:
-            client.connect((address, 8334))
-            logging.info("Connected to node [{}, {}]"
-                         .format(client.getpeername()[0], client.getpeername()[1]))
-        except ConnectionRefusedError:
-            logging.info("Connection attempt to node refused [{}, {}]"
-                         .format(address, port))
-            clients.remove(client)
-            client_addresses.remove(address)
-
-    logging.info("{} nodes accepted connection"
-                 .format(len(clients)))
+    threading.Thread(name="client connection attempts", target=initiate_clients,args=(peer_addresses, port, )).start()
+    del peer_addresses
 
     # main loop
 
-    inputs = []
+    inputs = [server_socket]
     outputs = []
     message_queue = {}
 
+    while inputs:
+        readable, writable, exceptional = select.select(inputs + client_sockets.array, outputs, inputs)
+
+        for sock in readable:
+            if sock is server_socket:
+                connection, client_address = server_socket.accept()
+                connection.setblocking(False)
+
+                inputs.append(connection)
+
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(threadName)s, %(asctime)s: %(message)s")
     main()
