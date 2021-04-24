92986247f06705400a0974a6ee4210ca216392cd
OrenSitton <64351837+OrenSitton@users.noreply.github.com>
2021-04-24 17:57:24 +0300

    documented configure.py

--- FullNode/Dependencies/configure.py
+++ FullNode/Dependencies/configure.py
@@ -2,21 +2,21 @@
 Author: Oren Sitton
 File: configure.py
 Python Version: 3.8
-Description: 
+Description: configure config.txt file for full node
 """
 from pickle import dump
 
 
 def main():
     configuration = {
-        "ip address":"localhost",
-        "port":8333,
-        "seed address":"localhost",
-        "seed port":8666,
-        "sql address":"localhost",
-        "sql port":3306
+        "ip address": "localhost",
+        "port": 8333,
+        "seed address": "localhost",
+        "seed port": 8666,
+        "sql address": "localhost",
+        "sql port": 3306
     }
-    with open ("config.txt", "wb") as file:
+    with open("config.txt", "wb") as file:
         dump(configuration, file)
 
 
