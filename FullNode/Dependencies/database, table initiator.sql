7b202f1615c556c8f8f897173023b2923628df46
OrenSitton <64351837+OrenSitton@users.noreply.github.com>
2021-04-24 18:27:09 +0300

    revised sql initializer

--- FullNode/Dependencies/database, table initiator.sql
+++ FullNode/Dependencies/database, table initiator.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE IF NOT EXISTS Blockchain;
-CREATE TABLE IF NOT EXISTS Blockchain.block (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, DATA LONGTEXT, prev_hash VARCHAR(64) NOT NULL);
+CREATE TABLE if not EXISTS Blocks (
+    id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    block_number INT UNSIGNED, time_created TIMESTAMP,
+    size MEDIUMINT, hash VARCHAR(64) NOT NULL, difficulty SMALLINT,
+    nonce MEDIUMINT,
+    merkle_root_hash VARCHAR(64),
+    transactions LONGBLOB,
+    self_hash VARCHAR(64));
