68db921490b75ccc51583fdd4a1cd04b5e6c62cc
OrenSitton <64351837+OrenSitton@users.noreply.github.com>
2021-05-03 13:24:13 +0300

    documented, added TypeErrors and fixed imports in Block.py

--- FullNode/Dependencies/Block.py
+++ FullNode/Dependencies/Block.py
@@ -2,51 +2,56 @@
 Author: Oren Sitton
 File: Block.py
 Python Version: 3
-Description: 
 """
 
 from Dependencies.Transaction import Transaction
-from hashlib import sha256
-
-
-def hexify(number, length):
-    """
-    calculates hexadecimal value of the number, with prefix zeroes to match length
-    :param number: number to calculate hex value for, in base 10
-    :type number: int
-    :param length: requested length of hexadecimal value
-    :type length: int
-    :return: hexadecimal value of the number, with prefix zeroes
-    :rtype: str
-    :raise Exception: ValueError (message size is larger than length)
-    """
-    if not isinstance(number, int):
-        raise TypeError("Transaction.hexify(number, length): expected number to be of type int")
-    if not isinstance(length, int):
-        raise TypeError("Transaction.hexify(number, length): expected length to be of type int")
-    if number < 0:
-        raise ValueError("Transaction.hexify(number, length): expected non-negative value for number, received {} "
-                         "instead".format(number))
-    if length < 0:
-        raise ValueError("Transaction.hexify(number, length): expected non-negative value for length, received {} "
-                         "instead".format(length))
-
-    hex_base = hex(number)[2:]
-
-    if len(hex_base) <= length:
-        hex_base = (length - len(hex_base)) * "0" + hex_base
-        return hex_base
-    else:
-        raise ValueError("Transaction.hexify(number, length): message size is larger than length")
-
-
-def calculate_hash(merkle_root_hash, prev_block_hash, nonce):
-    value = "{}{}{}".format(prev_block_hash, merkle_root_hash, nonce)
-    return sha256(value.encode()).hexdigest()
+from Dependencies.methods import calculate_hash, hexify
 
 
 class Block:
+    """
+    Block class, used to get and append blocks from Blockchain
+
+    Attributes
+    ----------
+    block_number : int
+
+    timestamp : int
+        posix timestamp of when block was created
+    difficulty : int
+        difficulty of the block
+    nonce : int
+        block's nonce
+    prev_hash : str
+        hash of the previous block in the blockchain
+    merkle_root_hash : str
+        merkle tree of the block's transactions root hash
+    transactions : list
+        list of the block's transactions
+    self_hash : str
+        hash of the block
+
+    Methods
+    -------
+    __init__(block)
+        initiator for Block object
+    network_format()
+        returns the block in the network format
+
+    Static Methods
+    --------------
+    from_network_format(message)
+        returns a Block object from a network format message
+    """
+
     def __init__(self, block):
+        """
+        initiator for Block object
+        :param block: block from Blockchain
+        :type block: tuple
+        """
+        if not isinstance(block, tuple):
+            raise TypeError("Block.__init__: expected block to be of type tuple")
         self.block_number = block[1]
         self.timestamp = block[2]
         self.difficulty = block[3]
@@ -59,6 +64,11 @@ class Block:
         self.self_hash = block[8]
 
     def network_format(self):
+        """
+        returns the Block in the network format
+        :return: block in the network format
+        :rtype: str
+        """
         network_format = "d{}{}{}{}{}{}{}".format(hexify(self.block_number, 2), hexify(self.timestamp, 8),
                                                   hexify(self.difficulty, 2), hexify(self.nonce, 64), self.prev_hash,
                                                   self.merkle_root_hash, hexify(len(self.transactions), 2))
@@ -68,6 +78,15 @@ class Block:
 
     @staticmethod
     def from_network_format(message):
+        """
+        returns a Block object from a network format message
+        :param message: block message
+        :type message: str
+        :return: Block object from network message
+        :rtype: Block
+        """
+        if not isinstance(message, str):
+            raise TypeError("Block.from_network_format: expected message to be of type str")
         if not message[0] == 'd':
             raise ValueError()
         block_number = int(message[1:7], 16)
---
+++ FullNode/Dependencies/methods.py
@@ -0,0 +1,14 @@
+"""
+Author: Oren Sitton
+File: methods.py
+Python Version: 3
+Description: 
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
